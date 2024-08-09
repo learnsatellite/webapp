7cc29b829c0169b468c302a5f4bbd85a63da80a4
learnsatellite <stkaji.sl@kaien-t.net>
2024-08-09 13:28:24 +0900

    fix bugs admin password

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,7 +1,43 @@
 from django.contrib import admin
 from todoapp.models import Task, User
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("email",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("username",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
 
 # Register your models here.
 admin.site.register(Task)
 
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
