db739f2f549a3c18292600c1c328112c58139b1d
learnsatellite <stkaji.sl@kaien-t.net>
2024-09-24 14:00:13 +0900

    DEBUG False

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "webappopen.pythonanywhere.com"]
 
